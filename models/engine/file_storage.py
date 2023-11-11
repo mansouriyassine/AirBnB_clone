82ba77d5fdb04ce13856dbbd4303958b2f01cac2
mansouriyassine <mansouri.yassine1@gmail.com>
2023-11-11 14:22:30 -0800

    8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
+# file_storage.py
 import json
+from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
@@ -14,7 +16,9 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
-        obj_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        obj_dict = {
+            k: v.to_dict() for k, v in FileStorage.__objects.items()
+        }
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
@@ -23,9 +27,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 objects = json.load(f)
             for k, v in objects.items():
-                cls_name = v['__class__']
-                from models.base_model import BaseModel
-                cls = eval(cls_name)
-                FileStorage.__objects[k] = cls(**v)
+                class_name = v['__class__']
+                if class_name == 'User':
+                    FileStorage.__objects[k] = User(**v)
+                elif class_name == 'BaseModel':
+                    FileStorage.__objects[k] = BaseModel(**v)
         except FileNotFoundError:
             pass
